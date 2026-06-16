b69958f6a93649672ea715f9a862de480acf8366
Chidibless <ngwutadavid507@gmail.com>
2026-06-16 20:56:50 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -17,11 +17,18 @@ app.add_middleware(
 def home():
     return {"status": "running"}
 
+@app.get("/health")
+def health():
+    return {
+        "status": "ok",
+        "ws_route": "/ws"
+    }
+
 @app.get("/symbols")
 def symbols():
     return [
         {"symbol": "BTCUSDT", "price": 65000},
-        {"symbol": "ETHUSDT", "price": 3500},
+        {"symbol": "ETHUSDT", "price": 3500}
     ]
 
 @app.websocket("/ws")
@@ -34,30 +41,13 @@ async def websocket_endpoint(ws: WebSocket):
             "data": [
                 {
                     "symbol": "BTCUSDT",
-                    "price": 65000 + int(time.time()) % 10
+                    "price": 65000 + (int(time.time()) % 10)
                 },
                 {
                     "symbol": "ETHUSDT",
-                    "price": 3500 + int(time.time()) % 5
+                    "price": 3500 + (int(time.time()) % 5)
                 }
             ]
         })
 
         await asyncio.sleep(1)
-@app.websocket("/ws")
-async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
-
-    while True:
-        await ws.send_json({
-            "status": "connected"
-        })
-
-        await asyncio.sleep(1)
-
-  @app.get("/health")
-def health():
-    return {
-        "ws_route": "/ws",
-        "status": "ok"
-    }      
