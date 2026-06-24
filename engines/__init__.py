fb22e9946fa8be23d713158bbbc887c6a71d50ab
Chidibless <ngwutadavid507@gmail.com>
2026-06-24 19:42:52 +0100

    Create __init__.py

---
+++ engines/__init__.py
@@ -0,0 +1,7 @@
+from .trend_engine        import analyse_trend
+from .momentum_engine     import analyse_momentum
+from .volatility_engine   import analyse_volatility
+from .volume_engine       import analyse_volume
+from .signal_engine       import generate_signal
+from .market_score_engine import compute_market_score
+from .whale_engine        import detect_whales, order_book_imbalance
